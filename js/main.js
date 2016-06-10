b161bfee370505f86bb378642d813a78653c6d5c
Julio Aenlle <gmg8r251@ufl.edu>
2016-06-10 15:21:43 -0400

    Add presidents object

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//presidents
+var presidents = [
+    {firstName: 'Barack', lastName: 'Obama', termLength: 8, party: 'Democratic', yearsOfPresidency: '2008 through 2016'},
+    {firstName: 'George', lastName: 'Bush', termLength: 8, party: 'Republican', yearsOfPresidency: '2000 through 2007'},
+    {firstName: 'Bill', lastName: 'Clinton', termLength: 8, party: 'Democratic', yearsOfPresidency: '1992 through 1999'},
+    {firstName: 'George', lastName: 'Bush', termLength: 4, party: 'Republican', yearsOfPresidency: '1988 through 1991'},
+    {firstName: 'Ronald', lastName: 'Reagan', termLength: 8, party: 'Republican', yearsOfPresidency: '1980 through 1987'}
+];
+console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
