5f7f615f939c6434c3842be210e3924568b203ae
Julio Aenlle <gmg8r251@ufl.edu>
2016-06-10 15:25:19 -0400

    Add howdy and numba functions

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//howdy
+
+function howdy() {
+    console.log('Julio', 'Aenlle', 'says sup!');
+}
+
+howdy();
+
+
+//numba
+
+var numba = function (string) {
+    if (string.length < 7) {
+        console.log('What a short little word!');
+    } else if (string.length > 7) {
+        console.log('I’m sorry, but that’s too many to count.');
+    } else {
+        console.log('7, what a perfect choice!');
+    }
+};
+(numba('7'));
+(numba('seventy'));
+(numba('university'));
+
+
+//inception
